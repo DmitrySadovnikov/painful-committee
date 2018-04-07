8598d53f4f4a72ba9b2d09cc7b118dfdeac7f2bf
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-04-07 21:19:56 +0300

    [tweet] Add supervisor

--- lib/pluralsight_tweet/application.ex
+++ lib/pluralsight_tweet/application.ex
@@ -10,11 +10,16 @@ defmodule PluralsightTweet.Application do
     children = [
       # Starts a worker by calling: PluralsightTweet.Worker.start_link(arg)
       # {PluralsightTweet.Worker, arg},
+      {PluralsightTweet.TweetServer, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PluralsightTweet.Supervisor]
-    Supervisor.start_link(children, opts)
+    process = Supervisor.start_link(children, opts)
+    PluralsightTweet.Scheduler.schedulet_file("* * * * *",
+    Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "sample.txt"))
+
+    process
   end
 end
--- lib/pluralsight_tweet/scheduler.ex
+++ lib/pluralsight_tweet/scheduler.ex
@@ -1,7 +1,10 @@
 defmodule PluralsightTweet.Scheduler do
   def schedulet_file(schedule, file) do
-    Quantum.add_job(schedule, fn -> IO.puts
-    PluralsightTweet.FileReader.get_strings_to_tweet(file)
-    end)
+    Quantum.add_job(
+      schedule,
+      fn -> PluralsightTweet.FileReader.get_strings_to_tweet(file)
+            |> PluralsightTweet.TweetServer.tweet
+      end
+    )
   end
 end
